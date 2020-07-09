264c20733d38fbd82347c70555fcac9d4c274821
unknown <allen030302@gmail.com>
2020-07-09 14:15:19 +0800

    first

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText=findViewById(R.id.edit);
